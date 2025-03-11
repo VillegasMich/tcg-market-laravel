782896c88086fbc8e9825b899dda1a86848c9b6b
VillegasMich <manuel.villegas.michel@gmail.com>
2025-03-11 12:40:53 -0500

    chore: many to many relationship TCGCards and WishList

---
+++ database/migrations/2025_03_11_173748_create_tcgcard_wishlist_pivot_table.php
@@ -0,0 +1,30 @@
+<?php
+
+// AUTHOR: Manuel Villegas Michel
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_c_g_card_wish_list', function (Blueprint $table) {
+            $table->id();
+            $table->integer('t_c_g_card_id')->unsigned(); // NOTE: Mandatory syntax
+            $table->integer('wish_list_id')->unsigned(); // NOTE: Mandatory syntax
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_c_g_card_wish_list');
+    }
+};
