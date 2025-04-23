4971bd5fbcdaa6155878006c9dd00fee8dfbec6c
VillegasMich <manuel.villegas.michel@gmail.com>
2025-04-23 09:26:47 -0500

    fix: .sh file deleted

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Link storage (only if not already a link)
-if [ ! -L /var/www/html/public/storage ]; then
-  php artisan storage:link
-fi
-
-# Continue with Apache
-apache2-foreground
